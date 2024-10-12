99bba4ca3e7956433519bad6ad8aa2f8885512e2
ArjunPathania <arjunpathania786f@gmail.com>
2024-10-12 17:43:32 +0530

    Add input check to QuizBrain.next_question() and comments to main.py

--- Day_17/main.py
+++ Day_17/main.py
@@ -2,15 +2,19 @@ from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
 
+# Creating a question bank
 question_bank = []
 for question in question_data:
-    new_question = Question(question['question'],question['correct_answer'])
+    new_question = Question(question['question'], question['correct_answer'])
     question_bank.append(new_question)
 
+# Initializing the quiz
 quiz = QuizBrain(question_bank)
 
+# Running the quiz
 while quiz.still_has_questions():
     quiz.next_question()
 
+# End of quiz
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
