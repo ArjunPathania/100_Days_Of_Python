6bc4244a8bb0220dd81b3201dcf3d7503778ffd6
ArjunPathania <arjunpathania786f@gmail.com>
2024-10-24 14:53:15 +0530

    added timer before game start added game length choice

--- Day_22/main.py
+++ Day_22/main.py
@@ -14,16 +14,18 @@ def setup_game():
     r_scoreboard = ScoreBoard((40, 250))
     return screen, l_paddle, r_paddle, ball, l_scoreboard, r_scoreboard
 
-def check_score(l_scoreboard, r_scoreboard, screen):
+def check_score(l_scoreboard, r_scoreboard, screen,game_length):
     """Checks if either player has won and asks if they want to play again."""
-    if l_scoreboard.score >= 5 or r_scoreboard.score >= 5:
-        winner = "Player One" if l_scoreboard.score >= 5 else "Player Two"
+    if l_scoreboard.score >= game_length or r_scoreboard.score >= game_length:
+        winner = "Player One" if l_scoreboard.score >= game_length else "Player Two"
         return screen.screen.textinput(title="Game Over", prompt=f"{winner} Won! Play Again? 'y' or 'n' :").lower()
     return None
 
 def game_loop(screen, l_paddle, r_paddle, ball, l_scoreboard, r_scoreboard):
     """Runs the main game loop."""
     is_game_on = True
+    game_length = int(screen.screen.textinput(title="Game Length",prompt="Enter game length: (5/10/15) points "))
+    time.sleep(3) # give time player to get ready
     while is_game_on:
         screen.screen.update()
         screen.screen.listen()
@@ -45,7 +47,7 @@ def game_loop(screen, l_paddle, r_paddle, ball, l_scoreboard, r_scoreboard):
         elif ball.xcor() < -390:
             r_scoreboard.update_score()
 
-        play_again = check_score(l_scoreboard, r_scoreboard, screen)
+        play_again = check_score(l_scoreboard, r_scoreboard, screen,game_length)
         if play_again == 'n':
             is_game_on = False
         elif play_again == 'y':
@@ -53,7 +55,6 @@ def game_loop(screen, l_paddle, r_paddle, ball, l_scoreboard, r_scoreboard):
             screen, l_paddle, r_paddle, ball, l_scoreboard, r_scoreboard = setup_game()
 
     screen.screen.exitonclick()
-
 
 def main():
     screen, l_paddle, r_paddle, ball, l_scoreboard, r_scoreboard = setup_game()
