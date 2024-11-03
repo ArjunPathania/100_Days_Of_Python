67a8a2a3a2c6c430abd9e3e86825458609528c46
ArjunPathania <arjunpathania786f@gmail.com>
2024-11-03 18:32:13 +0530

    added exception handling to day 26

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,17 +1,27 @@
 import pandas as pd
+
+# Load the NATO phonetic alphabet data
 df = pd.read_csv("nato_phonetic_alphabet.csv")
-nato_dict = {value.letter:value.code for (key,value) in df.iterrows()}
-# print(nato_dict)
+nato_dict = {value.letter: value.code for (key, value) in df.iterrows()}
 
-#.
-user_input = input("Enter a string:").upper()
-user_list = list(user_input)
-user_phonetic_string_list = [nato_dict[alphabet] for alphabet in user_list]
-print(user_phonetic_string_list)
+# Function to get phonetic code words
+def generate_phonetic():
+    user_input = input("Enter a string: ").upper()
+    try:
+        user_phonetic_string_list = [nato_dict[alphabet] for alphabet in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        return generate_phonetic()  # Recursively prompt again if invalid input
+    else:
+        print(user_phonetic_string_list)
 
-# # Keyword Method with iterrows()
-# # {new_key:new_value for (index, row) in df.iterrows()}
-#
+generate_phonetic()
+
+
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
 # import pandas
 #
 # data = pandas.read_csv("nato_phonetic_alphabet.csv")
